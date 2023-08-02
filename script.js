a7d2e260d7a895cabe9a97f626321179cd1bc96a
FedericoProgramando <fede_2398@hotmail.com>
2023-08-02 12:57:11 -0300

    Crombie

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const randomNumber = Math.floor(Math.random() * 100) + 1;
+let intentos = 0;
+
+function updateAttemptsDisplay() {
+    const attemptsDisplay = document.getElementById('intentos');
+    attemptsDisplay.innerHTML = `Número de intentos: ${intentos}`;
+}
 
 function checkGuess() {
     console.log("randomNumber", randomNumber);
     let guess = parseInt(document.getElementById('guessInput').value);
     let result = document.getElementById('result');
+    
+    intentos++;
 
-    if (guess === randomNumber) {
+    if (guess < -1) {
+        result.innerHTML = 'Valor ingresado es incorrecto. Debes ingresar un número mayor a 0.';
+    } else if (guess > 100) {
+        result.innerHTML = 'Valor ingresado es incorrecto. Debes ingresar un número menor a 100.';
+    } else if (guess === randomNumber) {
         result.innerHTML = '¡Felicitaciones! ¡Adivinaste el número!';
     } else if (guess < randomNumber) {
         result.innerHTML = 'Intenta con un número más alto.';
     } else {
         result.innerHTML = 'Intenta con un número más bajo.';
     }
+
+    updateAttemptsDisplay();
 }
